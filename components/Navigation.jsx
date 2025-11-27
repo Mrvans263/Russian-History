86506a1207266818f462f85f357392f5a54d3f86
Evans <www.evanschauke@gmail.com>
2025-11-27 07:28:14 +0300

    m

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,6 +1,6 @@
 // components/Navigation.js - UPDATED
 import React from 'react';
-import { useLanguage } from '../contexts/LanguageContext.jsx';
+import { useLanguage } from '../contexts/LanguageContext.jsx'
 import { LANGUAGES } from '../utils/Languages.jsx';
 import './Navigation.css';
 
