151a462cb3b28bc9d57c0190c9f60e59f9e8e062
Evans <www.evanschauke@gmail.com>
2025-11-27 06:23:54 +0300

    language context rename

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,6 +1,6 @@
 // components/Navigation.js - UPDATED
 import React from 'react';
-import { useLanguage } from '../contexts/LanguageContext';
+import { useLanguage } from '../contexts/languageContext';
 import { LANGUAGES } from '../utils/languages';
 import './Navigation.css';
 
