39cb1913a57293eb97855d57540afb2a9c9f0d14
Evans <www.evanschauke@gmail.com>
2026-05-19 14:57:57 +0300

    final

--- components/StudentDashBoard.jsx
+++ components/StudentDashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
-import './StudentDashboard.css';
+import './StudentDashBoard.css';
 
 const StudentDashBoard = ({ canUpload = false }) => {
   const [allSubmissions, setAllSubmissions] = useState([]);
